7c1c658b6d43604b9d1350a0dad74f9bac7e093c
Xie Zhongwei <472408570@qq.com>
2014-10-01 18:22:58 +0800

    KMP优化
    KMP优化

--- KMP.java
+++ KMP.java
@@ -20,7 +20,24 @@ public class KMP {
 			}
 		}
 	}
-
+		//KMP优化
+	public static void getBetterNext(String pattern,int[] next) {
+		char[] p = pattern.toCharArray();
+		int j=0,k = -1,len = next.length;		
+		while(j<len) {
+			if(j==-1||p[j]==p[k]) {
+				j++;
+				k++;
+				if(p[j]!=p[k])
+				  next[j] = k;
+				else
+				  next[j] = next[k];//如果p[j]==p[k],因为已经有p[j]不等于匹配串了，j= k后，p[j]还是不等于p[i]，所以next[j]= next[k];
+			}
+			else 
+			   k = next [k];
+		}
+		
+	}
 	public static int KmpMatch(String str1, String pattern) {
 		int[] next = new int[pattern.length()];
 		char[] s = str1.toCharArray();
