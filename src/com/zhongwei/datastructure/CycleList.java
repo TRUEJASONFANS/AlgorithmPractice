253e5903cca53015452d7c0813504e4425fedb78
Xie Zhongwei <472408570@qq.com>
2016-01-16 20:13:37 +0800

    Cycle list Demo

---
+++ src/com/zhongwei/datastructure/CycleList.java
@@ -0,0 +1,68 @@
+package com.zhongwei.datastructure;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+public class CycleList<T> {
+
+	private static int len = 2;
+	private int head = 0;
+	private int tail = 0;
+	private int curLen = 0;
+
+	private final List<T> array = Lists.newArrayList();
+	private final int[] fArray = new int[len];
+
+	public boolean enQueue(T t) throws Exception {
+		if (!full()) {
+			fArray[tail] = 1;
+			array.add(tail, t);
+			tail = (tail + 1) % len;
+			curLen++;
+			return true;
+		} else {
+			throw new Exception("the list do not contain any element");
+		}
+	}
+
+	public T deQueue() throws Exception {
+		if (!empty()) {
+			T t = array.get(head);
+			fArray[head] = 0;
+			head = (head + 1) % len;
+			curLen--;
+			return t;
+		} else {
+			throw new Exception("the list do not contain any element");
+		}
+	}
+
+	public boolean full() {
+		return curLen == len;
+	}
+
+	public boolean empty() {
+		return curLen == 0;
+	}
+
+	public void traverse() {
+		int i = 0, j = head;
+		while (i < curLen) {
+			System.out.println(array.get(j));
+			j = (j + 1) % len;
+			i++;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		CycleList<Integer> cycleList = new CycleList<>();
+		cycleList.enQueue(2);
+		cycleList.enQueue(3);
+		cycleList.deQueue();
+		cycleList.enQueue(4);
+		cycleList.traverse();
+	}
+	
+
+}
